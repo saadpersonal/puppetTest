e4630463e16370c789cc64206408c596e0609f70
Saad Khan <saadkhan340@hotmail.com>
2021-02-07 13:39:03 -0500

    Adding generated master public key to ssh profile

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDdy0lpANKXkJFUNvLXoGmDv6YgJcSyfPdZ7WdMP/wlDwNHiN5YsKR98gO68KdgiRELHtew8AE0Z03zlT8LNDKHph1ATPFPyR9oSrk53l9Pb3Q3fXvIENyc6OzZ6YzAewzWp3Mp6WXPAmws7jCX7oaRJv1EZImEtnFis9xzp1FUcvz7dMdmV8nsTXIoJWLrjDOsp/SfditqG7+PXnfwWr8GeV0eP9466o4o9EaLM9SBz98NIzz5Gj2VS3KrDZ9ucYol6GeS+73PfuWKnXACD5GoN6JWxH1CgYRfPqoJoLGkezE3EsQTm3DW0ZcGRzoslXfvz59SL/AVp8QwGZp9gOPr',
 	}  
 }
